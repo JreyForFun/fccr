4b3d88e1c7b5ec01571e9c417f1d77aee5d063c0
JreyForFun <jreyforfun@gmail.com>
2026-06-18 23:36:53 +0800

    init

---
+++ reusableFooter/index.jsx
@@ -0,0 +1,17 @@
+export function Footer(){
+  return (
+    <footer className="footerContainer">
+      <ul className="pageLinks">
+          <li><a href="#">foot</a></li>
+          <li><a href="#">foote</a></li>
+          <li><a href="#">footer</a></li>
+      </ul>
+      <p className="copyrights">© 2024 Fitness Dashboard. All Rights Reserved.</p>
+      <ul className="iconLinks">
+          <li><a href="#">🎮</a></li>
+          <li><a href="#">🏀</a></li>
+          <li><a href="#">💻</a></li>
+      </ul>
+    </footer>
+  )
+}
