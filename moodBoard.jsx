26a776b5054422592c076160d8a46a459e198d95
JreyForFun <jreyforfun@gmail.com>
2026-06-21 15:25:59 +0800

    update mB

--- moodBoard.jsx
+++ moodBoard.jsx
@@ -1,21 +1,21 @@
-export function MoodBoardItem({color, image, description}){
-  return (
-    <div className="mood-board-item" style={{backgroundColor: color}}>
-      <img className="mood-boar-image" src={image}></img>
-      <h3 className="mood-board-text">{description}</h3>
-    </div>
-  )
-}
+  export function MoodBoardItem({color, image, description}){
+    return (
+      <div className="mood-board-item" style={{backgroundColor: color}}>
+        <img className="mood-board-image" src={image}/>
+        <h3 className="mood-board-text">{description}</h3>
+      </div>
+    )
+  }
 
-export function MoodBoard(){
-  return (
-    <div className="mood-board">
-      <h1 className="mood-board-heading">Destination Mood Board</h1>
-      <>
-        <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg" description="jansdjnawjdnj"/>
-        <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg" description="jansdjnawjdnj"/>
-        <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/ship.jpg" description="jansdjnawjdnj"/>
-      </>
-    </div>
-  )
-}
+  export function MoodBoard(){
+    return (
+      <div className="mood-board">
+        <h1 className="mood-board-heading">Destination Mood Board</h1>
+        <>
+          <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg" description="jansdjnawjdnj"/>
+          <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg" description="jansdjnawjdnj"/>
+          <MoodBoardItem color="blue" image="https://cdn.freecodecamp.org/curriculum/labs/ship.jpg" description="jansdjnawjdnj"/>
+        </>
+      </div>
+    )
+  }
